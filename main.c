e0289430d20bec4e8efaf16a524332fe692231e4
Miguel Piñeiro <pineiro.c.s.mi@gmail.com>
2025-09-24 01:05:37 +0000

    fix: rigor teórico

--- main.c
+++ main.c
@@ -10,100 +10,127 @@ Thais Ferreira Canguçu           RA10403283
 #include <stdlib.h>
 #include <ctype.h>
 
-
+// O alfabeto é [0-9,+,-,.,,]
 char* scanner(char* palavra) {
     int index = 0;
 	char c;
 	
 	inicial_q0:
 		c = palavra[index++];
-	    if (isdigit(c) && c != '0') goto inteiro_q2;
-		else if (c == '0') goto zero_unico_q11;
-        else if (c == '+' || c == '-') goto sinal_q1;
-		else return "<REJEITADO>";
+	    if (isdigit(c) && c != '0') goto inteiro_q2;  // n diferente de 0
+		else if (c == '0') goto zero_unico_q11;       // 0
+        else if (c == '+' || c == '-') goto sinal_q1; // sinais
+		else if (c == ',') goto poco;                 // vírgula 
+		if (c == '\0') return "<REJEITADO>";          // Não é estado final
+		else goto poco;
 
 	sinal_q1:
 		c = palavra[index++];
-		if (isdigit(c) && c != '0') goto inteiro_com_sinal_q6;
-		else if (c != '0') goto zero_pos_sinal_inteiro_q10;
-        else return "<REJEITADO>";
+		if (isdigit(c) && c != '0') goto inteiro_com_sinal_q6; // n diferente de 0
+		else if (c == '0') goto zero_pos_sinal_inteiro_q10;    // zero
+		else if (c == ',' || c == '+' || c == '-') goto poco;  // Simbolo ou virgula
+		if (c == '\0') return "<REJEITADO>";                   // Não é estado final
+        else goto poco;                    
 	
 	inteiro_q2:
-    	c = palavra[index++];
-		if (c == '\0') return "<INTEIRO>";
-	    else if (isdigit(c)) goto inteiro_q2;
-        else if (c == ',') goto virgula_q4;
-        else return "<REJEITADO>";
+    	c = palavra[index++];              
+		if (c == '\0') return "<INTEIRO>";          // Final
+	    else if (isdigit(c)) goto inteiro_q2;       // Qualquer dígito
+        else if (c == ',') goto virgula_q4;         // Vírgula
+		else if (c == '+' || c == '-') goto poco;   // Simbolo ou virgula
+        else goto poco;          
 
 	flutuante_q3:
-	    c = palavra[index++];
-	    if (c == '\0') return "<P.FLUTUANTE>";
-        else if (c == '0') goto zero_flutuante_q5;
-	    else if (isdigit(c) && c != '0') goto flutuante_q3;
-		else return "<REJEITADO>";
+	    c = palavra[index++];                       
+	    if (c == '\0') return "<P.FLUTUANTE>";                // Final 
+        else if (c == '0') goto zero_flutuante_q5;            // Zero
+	    else if (isdigit(c) && c != '0') goto flutuante_q3;   // n diferente de 0
+		else if (c == ',' || c == '+' || c == '-') goto poco; // virgula e simbolos
+		else goto poco;
 	    
 	virgula_q4:
 	    c = palavra[index++];
-	    if (c == '0') goto zero_flutuante_q5;
-	    else if (isdigit(c) && c != '0')  goto flutuante_q3;
-	    else return "<REJEITADO>";
+	    if (c == '0') goto zero_flutuante_q5;                  // Zero
+	    else if (isdigit(c) && c != '0')  goto flutuante_q3;   // n diferente de 0
+		else if (c == ',' || c == '+' || c == '-') goto poco;  // virgulas e simbolos
+		else if (c == '\0') return "<REJEITADO>";                   // Não é estado final
+	    else goto poco;
 	
 	zero_flutuante_q5:
 	    c = palavra[index++];
-	    if (c == '0') goto zero_flutuante_q5;
-	    else if (isdigit(c) && c != '0')  goto flutuante_q3;
-		else return "<REJEITADO>";
+	    if (c == '0') goto zero_flutuante_q5;                  // Zero
+	    else if (isdigit(c) && c != '0')  goto flutuante_q3;   // n diferente de 0
+		else if (c == ',' || c == '+' || c == '-') goto poco;  // virgulas e simbolos	
+		else if (c == '\0') return "<REJEITADO>";                   // Não é estado final
+		else goto poco;
 		
 		
 	inteiro_com_sinal_q6:
     	c = palavra[index++];
-		if (c == '\0') return "<INTEIRO COM SINAL>";
-	    if (isdigit(c)) goto inteiro_com_sinal_q6;
-        else if (c == ',') goto virgula_com_sinal_q8;
-        else return "<REJEITADO>";
+		if (c == '\0') return "<INTEIRO COM SINAL>";         // Final
+	    else if (isdigit(c)) goto inteiro_com_sinal_q6;           // Qualquer n
+        else if (c == ',') goto virgula_com_sinal_q8;        // vírgula 
+		else if (c == '+' || c == '-') goto poco;  			// simbolos	
+        else goto poco;
 
 	    
 	flutuante_com_sinal_q7:
 	    c = palavra[index++];
-	    if (c == '\0') return "<P.FLUTUANTE COM SINAL>";
-        else if (c == '0') goto zero_flutuante_com_sinal_q9;
-	    else if (isdigit(c) && c != '0') goto flutuante_com_sinal_q7;
-		else return "<REJEITADO>";
+	    if (c == '\0') return "<P.FLUTUANTE COM SINAL>";			  // Final
+        else if (c == '0') goto zero_flutuante_com_sinal_q9;          // zero
+	    else if (isdigit(c) && c != '0') goto flutuante_com_sinal_q7; // n diferente de 0
+		else if (c == ',' || c == '+' || c == '-') goto poco;         // virgulas e simbolos	
+		else goto poco;
 	    
 	virgula_com_sinal_q8:
-	    c = palavra[index++];
-	    if (c == '0') goto zero_flutuante_com_sinal_q9;
-	    else if (isdigit(c) && c != '0')  goto flutuante_com_sinal_q7;
-	    else return "<REJEITADO>";
+	    c = palavra[index++];											
+	    if (c == '0') goto zero_flutuante_com_sinal_q9;					// Zero
+	    else if (isdigit(c) && c != '0')  goto flutuante_com_sinal_q7;  // n diferente de 0
+	    else if (c == ',' || c == '+' || c == '-') goto poco;  			// virgulas e simbolos
+		else if (c == '\0') return "<REJEITADO>";                       // Não é estado final
+		else goto poco;
 	
 	zero_flutuante_com_sinal_q9:
 	    c = palavra[index++];
-	    if (c == '0') goto zero_flutuante_com_sinal_q9;
-	    else if (isdigit(c) && c != '0')  goto flutuante_com_sinal_q7;
-		else return "<REJEITADO>";
+	    if (c == '0') goto zero_flutuante_com_sinal_q9;                  // Zero
+	    else if (isdigit(c) && c != '0')  goto flutuante_com_sinal_q7;   // n diferente de 0
+		else if (c == ',' || c == '+' || c == '-') goto poco;  			 // virgulas e simbolos
+		else if (c == '\0') return "<REJEITADO>";                        // Não é estado final
+		else goto poco;
 	
 	zero_pos_sinal_inteiro_q10:
 	    c = palavra[index++];
-	    if (c == '0') goto zero_pos_sinal_inteiro_q10;
-	    else if (c == ',') goto virgula_com_sinal_q8;
-	    else if (isdigit(c) && c != '0')  goto inteiro_com_sinal_q6;
-		else return "<REJEITADO>";
+	    if (c == '0') goto zero_pos_sinal_inteiro_q10;                   // Zero
+	    else if (c == ',') goto virgula_com_sinal_q8;                    // Vírgula
+	    else if (isdigit(c) && c != '0')  goto inteiro_com_sinal_q6;     // n diferente de 0
+		else if (c == '+' || c == '-') goto poco;  			             // Simbolos
+		else if (c == '\0') return "<REJEITADO>";                        // Não é estado final
+		else goto poco;
 	
 	zero_unico_q11:
 		c = palavra[index++];
-			if (c == '\0') return "<INTEIRO>";
-			else if (isdigit(c) && c != '0') goto inteiro_q2;
-			else if (c == '0') goto zeros_sequencia_q12;
- 			else if (c == ',') goto virgula_q4;
-			else return "<REJEITADO>";
+		if (c == '\0') return "<INTEIRO>";                  // Final
+		else if (isdigit(c) && c != '0') goto inteiro_q2;   // n diferente de 0
+		else if (c == '0') goto zeros_sequencia_q12;        // 0  
+		else if (c == ',') goto virgula_q4;                 // virgula
+		else if (c == '+' || c == '-') goto poco;  			// Simbolos
+		else goto poco;
 				
 	zeros_sequencia_q12:
 		c = palavra[index++];
-			if (c == '\0') return "<REJEITADO>";
-			else if (isdigit(c) && c != '0') goto inteiro_q2;
-			else if (c == '0') goto zeros_sequencia_q12; 
-			else if (c == ',') goto virgula_q4;
-			else return "<REJEITADO>";
+		if (c == '\0') return "<REJEITADO>";			  // Não é final
+		else if (isdigit(c) && c != '0') goto inteiro_q2; // n diferente de 0
+		else if (c == '0') goto zeros_sequencia_q12;      // 0
+		else if (c == ',') goto virgula_q4;               // vírgula 
+		else if (c == '+' || c == '-') goto poco;  		  // Simbolos
+		else goto poco;
+	
+	poco: 
+		c = palavra[index++];
+		if (isdigit(c) || c == '+' || c == '-' || c == ',') goto poco;
+		else if (c == '\0') return "<REJEITADO>";
+		else return "<REJEITADO>";
+
 }
 
 
